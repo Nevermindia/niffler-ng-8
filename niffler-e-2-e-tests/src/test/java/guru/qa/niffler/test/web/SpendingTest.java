e728264dd92008de7ad75c1bf20d7bf10929a7bd
apetkevich <apetkevich@sberhealth.ru>
2025-04-04 14:16:36 +0300

    homework_3_3

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/SpendingTest.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/SpendingTest.java
@@ -2,8 +2,9 @@ package guru.qa.niffler.test.web;
 
 import com.codeborne.selenide.Selenide;
 import guru.qa.niffler.config.Config;
+import guru.qa.niffler.jupiter.annotation.meta.User;
 import guru.qa.niffler.jupiter.annotation.meta.WebTest;
-import guru.qa.niffler.jupiter.annotation.Spend;
+import guru.qa.niffler.jupiter.annotation.Spending;
 import guru.qa.niffler.model.CurrencyValues;
 import guru.qa.niffler.model.SpendJson;
 import guru.qa.niffler.page.LoginPage;
@@ -13,24 +14,25 @@ import org.junit.jupiter.api.Test;
 @WebTest
 public class SpendingTest {
 
-  private static final Config CFG = Config.getInstance();
+    private static final Config CFG = Config.getInstance();
 
-  @Spend(
-      username = "nevermindia",
-      category = "Еда",
-      description = "Сырки Б Ю Александров",
-      amount = 1250.00,
-      currency = CurrencyValues.RUB
-  )
-  @Test
-  void spendingDescriptionShouldBeUpdatedByTableAction(SpendJson spend) {
-    final String newDescription = "Обучение Niffler NG";
+    @User(username = "nevermindia",
+            spendings = @Spending(
+                    category = "Еда",
+                    description = "Сырки Б Ю Александров",
+                    amount = 1250.00,
+                    currency = CurrencyValues.RUB
+            ))
 
-    Selenide.open(CFG.frontUrl(), LoginPage.class)
-        .doLogin("nevermindia", "nevermindia")
-        .editSpending(spend.description())
-        .editDescription(newDescription);
+    @Test
+    void spendingDescriptionShouldBeUpdatedByTableAction(SpendJson spend) {
+        final String newDescription = "Обучение Niffler NG";
 
-    new MainPage().checkThatTableContains(newDescription);
-  }
+        Selenide.open(CFG.frontUrl(), LoginPage.class)
+                .doLogin("nevermindia", "nevermindia")
+                .editSpending(spend.description())
+                .editDescription(newDescription);
+
+        new MainPage().checkThatTableContains(newDescription);
+    }
 }
