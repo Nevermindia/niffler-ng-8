eaedd7525f0e96fc0ed552efbc0e69b2cd5e15be
apetkevich <apetkevich@sberhealth.ru>
2025-04-09 12:51:51 +0300

    homework_4_1

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/jupiter/extension/CategoryExtension.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/jupiter/extension/CategoryExtension.java
@@ -3,6 +3,7 @@ package guru.qa.niffler.jupiter.extension;
 import guru.qa.niffler.api.SpendApiClient;
 import guru.qa.niffler.jupiter.annotation.meta.User;
 import guru.qa.niffler.model.CategoryJson;
+import guru.qa.niffler.service.SpendDbClient;
 import guru.qa.niffler.test.web.utils.RandomDataUtils;
 import org.apache.commons.lang3.ArrayUtils;
 import org.junit.jupiter.api.extension.*;
@@ -11,6 +12,7 @@ import org.junit.platform.commons.support.AnnotationSupport;
 public class CategoryExtension implements BeforeEachCallback, ParameterResolver, AfterTestExecutionCallback {
     public static final ExtensionContext.Namespace NAMESPACE = ExtensionContext.Namespace.create(CategoryExtension.class);
     private final SpendApiClient spendApiClient = new SpendApiClient();
+    private final SpendDbClient spendDbClient = new SpendDbClient();
 
     @Override
     public void beforeEach(ExtensionContext context) throws Exception {
@@ -21,18 +23,9 @@ public class CategoryExtension implements BeforeEachCallback, ParameterResolver,
                             null,
                             RandomDataUtils.randomCategoryName(),
                             anno.username(),
-                            true
+                            anno.categories()[0].archived()
                     );
-                    CategoryJson createdCategory = spendApiClient.addCategory(categoryJson);
-                    if (anno.categories()[0].archived()) {
-                        CategoryJson archivedCategory = new CategoryJson(
-                                createdCategory.id(),
-                                createdCategory.name(),
-                                createdCategory.username(),
-                                true
-                        );
-                        createdCategory = spendApiClient.updateCategory(archivedCategory);
-                    }
+                    CategoryJson createdCategory = spendDbClient.createCategory(categoryJson);
                     context.getStore(NAMESPACE).put(context.getUniqueId(), createdCategory);
                 });
     }
