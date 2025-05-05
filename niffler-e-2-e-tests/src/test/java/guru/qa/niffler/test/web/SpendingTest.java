b8a6ad80dcbd805051ba164e1b51efdbabfbaab6
apetkevich <apetkevich@sberhealth.ru>
2025-05-05 21:36:51 +0300

    lesson_9_1

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/SpendingTest.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/SpendingTest.java
@@ -2,6 +2,7 @@ package guru.qa.niffler.test.web;
 
 import com.codeborne.selenide.Selenide;
 import guru.qa.niffler.config.Config;
+import guru.qa.niffler.jupiter.annotation.ScreenShotTest;
 import guru.qa.niffler.jupiter.annotation.meta.User;
 import guru.qa.niffler.jupiter.annotation.meta.WebTest;
 import guru.qa.niffler.jupiter.annotation.Spending;
@@ -9,7 +10,22 @@ import guru.qa.niffler.model.CurrencyValues;
 import guru.qa.niffler.model.UserJson;
 import guru.qa.niffler.page.LoginPage;
 import guru.qa.niffler.page.MainPage;
+import guru.qa.niffler.test.web.utils.ScreenDiffResult;
+import io.qameta.allure.Allure;
 import org.junit.jupiter.api.Test;
+import org.springframework.core.io.ClassPathResource;
+import ru.yandex.qatools.ashot.comparison.ImageDiff;
+import ru.yandex.qatools.ashot.comparison.ImageDiffer;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+import static com.codeborne.selenide.Selenide.$;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 @WebTest
 public class SpendingTest {
@@ -34,5 +50,23 @@ public class SpendingTest {
                 .editDescription(newDescription);
 
         new MainPage().checkThatTableContains(newDescription);
+    }
+
+    @User(
+            spendings = @Spending(
+                    category = "Еда",
+                    description = "Сырки Б Ю Александров",
+                    amount = 1250.00,
+                    currency = CurrencyValues.RUB
+            ))
+    @ScreenShotTest("img/expected-stat.png")
+    void checkStatComponentTest(UserJson user, BufferedImage expected) throws IOException {
+        final String newDescription = "Обучение Niffler NG";
+
+        Selenide.open(CFG.frontUrl(), LoginPage.class)
+                .doLogin(user.username(), user.testData().password());
+        BufferedImage actual = ImageIO.read($("canvas[role='img']").screenshot());
+        assertFalse(new ScreenDiffResult(expected, actual));
+
     }
 }
