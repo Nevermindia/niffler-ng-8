4ea23164d2a439658eb807d14254558fbda226b6
apetkevich <apetkevich@sberhealth.ru>
2025-05-23 10:21:12 +0300

    homework_10_2

---
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/utils/BrowserConverterUtils.java
@@ -0,0 +1,23 @@
+package guru.qa.niffler.test.web.utils;
+
+import com.codeborne.selenide.SelenideDriver;
+import guru.qa.niffler.model.Browser;
+import org.junit.jupiter.api.extension.ParameterContext;
+import org.junit.jupiter.params.converter.ArgumentConversionException;
+import org.junit.jupiter.params.converter.ArgumentConverter;
+
+import static guru.qa.niffler.model.Browser.*;
+
+public class BrowserConverterUtils implements ArgumentConverter {
+    @Override
+    public Object convert(Object source, ParameterContext context) throws ArgumentConversionException {
+        if (!(source instanceof Browser)){
+            throw new ArgumentConversionException("Source must be instance of enum Browser");
+        }
+        return switch (source) {
+            case CHROME -> new SelenideDriver(SelenideUtils.chromeConfig);
+            case FIREFOX -> new SelenideDriver(SelenideUtils.firefoxConfig);
+            default -> throw new ArgumentConversionException("Unsupported browser type: " + source);
+        };
+    }
+}
